5f67d00136392255245e942006d8ac69372a94bf
0xtojifushiguro <60061293+0xtojifushiguro@users.noreply.github.com>
2026-02-23 02:41:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const readline = require('readline');
+const { ethers } = require('ethers');
+const dotenv = require('dotenv');
+const axios = require('axios');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+
+dotenv.config();
+
+const colors = {
+    reset: "\x1b[0m",
+    cyan: "\x1b[36m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    red: "\x1b[31m",
+    white: "\x1b[37m",
+    bold: "\x1b[1m",
+};
 
